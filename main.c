940ece343f909a93c822a789cbb856073e82c218
grrayya <grrayya11@gmail.com>
2026-06-29 23:13:48 -0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("========================================\n");
+    printf("[SYSTEM] INITIALIZATION COMPLETE.\n");
+    printf("[SYSTEM] WELCOME, PLAYER.\n");
+    printf("========================================\n\n");
+    
+    printf("You wake up in a dark room. Your head is pounding.\n");
+    printf("A glowing blue interface hovers in the air in front of you.\n");
+    
+    return 0;
+}
